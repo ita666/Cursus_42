c1ea0409d988904581444ad4519c50b4c2c8d19e
ita666 <55233818+ita666@users.noreply.github.com>
2022-05-08 13:44:47 +0200

    Add files via upload

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -15,28 +15,28 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
+	size_t	total;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	if (nmemb != 0 && ((nmemb * size) / nmemb != size))
-		return (NULL);
-	ptr = malloc(nmemb * size);
+	total = nmemb * size;
+	ptr = malloc(total);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, nmemb * size);
+	ft_bzero(ptr, total);
 	return (ptr);
 }
 /*
 #include <stdio.h>
+
 int main (void)
 {
  
- char *c  = "fififi";
+ //char *c  = "fififi";
  char *c1 = "fififi";
  
- c  = ft_calloc(4,2);
- c1 = calloc(4,2);
+ //c  = ft_calloc(4, 1);
+ c1 = calloc(4, 1);
 
- printf("%s\n", c);
+ //printf("%s\n", c);
  printf("%s\n", c1);
-}*/
+}
+*/
